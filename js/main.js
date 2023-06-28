1f47d49bf6eeaaccd5644d1ffee410016cf53578
Facundo <facundo.troitero@fedrax.com>
2023-06-28 11:27:52 +0100

    added webp format, added projects, fixed links, removed unused files

--- js/main.js
+++ js/main.js
@@ -40,26 +40,6 @@ $(function() {
     $('html, body').animate({ scrollTop: 0 }, 500);
   });
 
-  const hand = document.querySelector('.emoji.wave-hand');
-
-  function waveOnLoad() {
-    hand.classList.add('wave');
-    setTimeout(function() {
-      hand.classList.remove('wave');
-    }, 2000);
-  }
-
-  setTimeout(function() {
-    waveOnLoad();
-  }, 1000);
-
-  hand.addEventListener('mouseover', function() {
-    hand.classList.add('wave');
-  });
-
-  hand.addEventListener('mouseout', function() {
-    hand.classList.remove('wave');
-  });
 
   window.sr = ScrollReveal({
     reset: false,
